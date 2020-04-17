d1127a78b8010776e18182fd13b89cc17d441bc1
AdvancedMical <micalhl@sina.cn>
2020-04-17 18:45:47 +0800

    Initial commit

---
+++ src/main/java/co/mc520/house/House.java
@@ -0,0 +1,42 @@
+package co.mc520.house;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class House extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        super.onEnable();
+        this.saveDefaultConfig();
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        super.onDisable();
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args, PlayerEvent event) {
+        Player player = event.getPlayer();
+        if (command.getName().equalsIgnoreCase("house")){
+            if (!(sender instanceof Player)){
+                sender.sendMessage("&9&l家园系统&e&l>> &e只有玩家才可以执行此指令");
+            }else {
+                if (sender.hasPermission("house.use")) {
+                    Bukkit.getServer().dispatchCommand((CommandSender) player, getConfig().getString("command"));
+                } else {
+                    sender.sendMessage("&9&l家园系统&c&l>> &c您没有权限这样做");
+                }
+            }
+        }
+        return false;
+    }
+}
